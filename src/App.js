9d7687ee56ab0451117df105b49ac945d8e7817a
Paul <paul_carkner@hotmail.com>
2022-06-30 19:58:02 -0400

    1

--- src/App.js
+++ src/App.js
@@ -3,8 +3,20 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Hello Testing</h1>
       <h2>Start editing to see some magic happen!</h2>
+    <Map />
     </div>
   );
+}
+
+class Map extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (<div>hello you!</div>);
+    }
 }
